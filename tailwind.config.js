fdc2729cb029e8d14a1dec1ef5012b1779582f8d
Simon Aguirre <siaguirre@uade.edu.ar>
2024-09-18 13:26:05 -0300

    tailwind config, footer e index
    Se eliminan todos los archivos de Ingenia.
    Se agregan package.json
    Se modifica index.html a sobre-nosotros y se crea nuevo archivo index
    Se sube configuracion de tailwind con colores e imagen

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./*.{html, js}"],
+  theme: {
+    extend: {
+      colors: {
+        'brand-blue': '#253F55',
+        'bg-black': '#09090B',
+        'container-white': '#FCFCFC'
+      },
+      backgroundImage: {
+        'bg-footer': "url('../assets/img/degrade-footer.png')"
+      }
+    },
+  },
+  plugins: [],
+}
